861ebf9ddd5a60a060f8b051def84e643750cba7
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-02-16 15:50:47 +0900

    Init系のファイルをもう1つ追加した

--- Calcal/MainViewController/ExMainVC_Init.swift
+++ Calcal/MainViewController/ExMainVC_Init.swift
@@ -60,7 +60,7 @@ extension MainViewController {
       button.isUserInteractionEnabled = true
    }
    
-   //+とか-とかと=をかを初期化する
+   //一番上のラベルを初期化する
    func InitFormation() {
       let Wide = Size.width / 4
       let Interval = Size.width / 16
@@ -116,5 +116,96 @@ extension MainViewController {
       InitEarchNumberLabel(SecondLabel, 2)
       InitEarchNumberLabel(ThirdLabel, 3)
       InitEarchNumberLabel(ForthLabel, 4)
+   }
+   
+   func InitEarchButton(_ SetButton: UIButton, _ Num: Int){
+      let Wide = Size.width / 5
+      let Interval = Size.width / 25
+      
+      SetButton.frame = CGRect(x: Interval * CGFloat(Num) + Wide * CGFloat(Num - 1), y: Size.height * 3 / 4, width: Wide, height: Wide)
+      SetButton.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1)
+      SetButton.setTitle("+", for: .normal)
+      SetButton.titleLabel?.font = UIFont(name: "Helvetica", size: 30)!
+
+      SetButton.setTitleColor(UIColor.black, for: .normal)
+      SetButton.layer.borderColor = UIColor.black.cgColor
+      SetButton.layer.borderWidth = 2
+      SetButton.layer.masksToBounds = true
+      SetButton.layer.cornerRadius = 10
+      //SetButton.textAlignment = NSTextAlignment.center
+      SetButton.layer.cornerRadius = 10.0
+      SetButton.layer.borderColor = UIColor.black.cgColor
+      switch Num {
+      case 1:
+         SetButton.setTitle("+", for: .normal)
+         SetButton.addTarget(self, action: #selector(self.TapAdd), for: .touchUpInside)
+      case 2:
+         SetButton.setTitle("-", for: .normal)
+         SetButton.addTarget(self, action: #selector(self.TapSub), for: .touchUpInside)
+      case 3:
+         SetButton.setTitle("*", for: .normal)
+         SetButton.addTarget(self, action: #selector(self.TapMul), for: .touchUpInside)
+      case 4:
+         SetButton.setTitle("/", for: .normal)
+         SetButton.addTarget(self, action: #selector(self.TapDiv), for: .touchUpInside)
+      default:
+         print("err")
+      }
+      view.addSubview(SetButton)
+      
+      
+      
+   }
+   
+   func InitEarchLabel(_ SetLabel: UILabel, _ Num: Int) {
+   
+      let Wide = Size.width / 5
+      let Interval = Size.width / 25
+      
+      SetLabel.frame = CGRect(x: Interval * CGFloat(Num) + Wide * CGFloat(Num - 1), y: Size.height * 3 / 4, width: Wide, height: Wide)
+      SetLabel.font = UIFont(name: "Helvetica", size: 30)
+      SetLabel.layer.borderColor = UIColor.black.cgColor
+      SetLabel.layer.borderWidth = 2
+      SetLabel.layer.masksToBounds = true
+      SetLabel.layer.cornerRadius = 10
+      SetLabel.textAlignment = NSTextAlignment.center
+      SetLabel.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1)
+      SetLabel.layer.cornerRadius = 10.0
+      SetLabel.layer.borderColor = UIColor.black.cgColor
+      SetLabel.isUserInteractionEnabled = true
+      SetLabel.tag = (Num + 10)
+      switch Num {
+      case 1:
+      SetLabel.text = "+"
+      case 2:
+      SetLabel.text = "-"
+      case 3:
+      SetLabel.text = "×"
+      case 4:
+      SetLabel.text = "÷"
+      default:
+      print("error")
+      }
+      SetLabel.isHidden = true
+      view.addSubview(SetLabel)
+   }
+   
+   func InitArithmeticButton() {
+      InitEarchButton(Add, 1)
+      InitEarchButton(Sub, 2)
+      InitEarchButton(Mul, 3)
+      InitEarchButton(Div, 4)
+      InitEarchLabel(AddLabel, 1)
+      InitEarchLabel(AddLabel2, 1)
+      InitEarchLabel(AddLabel3, 1)
+      InitEarchLabel(SubLabel, 2)
+      InitEarchLabel(SubLabel2, 2)
+      InitEarchLabel(SubLabel3, 2)
+      InitEarchLabel(MulLabel, 3)
+      InitEarchLabel(MulLabel2, 3)
+      InitEarchLabel(MulLabel3, 3)
+      InitEarchLabel(DivLabel, 4)
+      InitEarchLabel(DivLabel2, 4)
+      InitEarchLabel(DivLabel3, 4)
    }
 }
